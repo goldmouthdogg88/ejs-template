af87554921a7d2115f806fde1992c5395348da1e
goldmouthdogg88 <goldmouthdogg88@gmail.com>
2022-04-24 19:55:19 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# blog
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/goldmouthdogg88/pen/popmoyp](https://codepen.io/goldmouthdogg88/pen/popmoyp).
+
---
+++ dist/script.js
@@ -0,0 +1,7 @@
+const resource = [
+  {
+    "title": "",
+    "url": "https://www.quackit.com/html/howto/how_to_make_a_background_image_not_repeat.cfm#:~:text=To%20make%20a%20background%20image%20not%20repeat%20in%20HTML%2C%20specify,or%20the%20background%20shorthand%20property.&text=%E2%80%8B-,The%20background%20image%20is%20set%20to%20'no%2Drepeat'%20using,'background%2Drepeat'%20property.",
+    "description": ""
+  }
+]
