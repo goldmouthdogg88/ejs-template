b8ac079433463e8b25449651f08fe284142875b0
PTRN317 <ptrn317@iu.edu>
2022-04-29 00:13:44 -0400

    first commit

--- src/script.js
+++ src/script.js
@@ -1,7 +1,30 @@
-const resource = [
+// import { writeFile } from "fs";
+const fs = require("fs");
+
+posts = [
   {
-    "title": "",
-    "url": "https://www.quackit.com/html/howto/how_to_make_a_background_image_not_repeat.cfm#:~:text=To%20make%20a%20background%20image%20not%20repeat%20in%20HTML%2C%20specify,or%20the%20background%20shorthand%20property.&text=%E2%80%8B-,The%20background%20image%20is%20set%20to%20'no%2Drepeat'%20using,'background%2Drepeat'%20property.",
-    "description": ""
+    title: "",
+    path: "",
+  },
+];
+
+function dynamicallyGeneratePosts() {}
+function createPage() {}
+function deletePage() {}
+
+// const content, path, fileName, extension;
+
+config = {
+  content: "Some content",
+  path: "./posts/",
+  fileName: "blog-post",
+};
+const { content, path, fileName } = config;
+
+fs.writeFile(`${path}${fileName}`, content, (err) => {
+  if (err) {
+    console.error(err);
   }
-]
+  // file written successfully
+  console.log("File successfully written");
+});
